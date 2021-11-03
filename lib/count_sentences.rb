edac9296558e5617c574c68286b920833ea2bed8
D-Justice <justice.damian96@gmail.com>
2021-11-04 09:10:55 +1100

    Assignment Complete

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    count = 0
+    self.split(/\?|\.|!/).each do |word|
+      word.size > 0 ? count += 1 : count
+    end
+    count
   end
 end
